37178a11ac9fc32e882e886a2fdba07796d283d5
Roneesh Vashisht <roneesh@gmail.com>
2012-12-17 13:26:16 -0600

    did db reset and created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Destroying all users..."
+User.destroy_all
+puts "Users destroyed!"
+
+puts "Destroying all Alphas..."
+Alpha.destroy_all
+puts "Alphas destroyed!"
+
+puts "Destroying all Microposts..."
+Micropost.destroy_all
+puts "Microposts destroyed!"
+
+users = [
+	{name: "Roneesh Vashisht", email: "roneesh@gmail.com", handle: "Ronnie", password: "1", password_confirmation: "1"},
+	{name: "Password User", email: "password@example.com", handle: "Passy", password: "1", password_confirmation: "1"}
+]
+
+puts "Creating users..."
+User.create users
+puts "Users created!"
+
+alphas = [
+	{name: "Foxbase 1"},
+	{name: "Foxbase 2"},
+	{name: "Foxbase 3"}
+]
+
+puts "Creating Alphas"
+Alpha.create alphas
+puts "Alphas created"
