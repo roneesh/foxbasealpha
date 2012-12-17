a85b88cf8da3b82ac5a2b718e64e455ca87bae9a
Roneesh Vashisht <roneesh@gmail.com>
2012-12-16 21:13:12 -0600

    scaffolding basic users and microposts per Hartl

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foxbasealpha::Application.config.secret_token = '0211eee9900e6d8f39f1a01a622d57d88584322f94289386d6463ce06aa7ad7e3148a89fdd7bf9c27de86deee1b0d94553cf06bb9ceeacd7ae381f61c4cf9104'
