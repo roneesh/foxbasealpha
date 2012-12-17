779570f1441732c2f873ee857ee5bd61a072135d
Roneesh Vashisht <roneesh@gmail.com>
2012-12-16 22:07:11 -0600

    adding handle to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121217033300) do
+ActiveRecord::Schema.define(:version => 20121217040402) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20121217033300) do
     t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "handle"
   end
 
 end
